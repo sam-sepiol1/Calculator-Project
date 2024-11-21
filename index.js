5d3163eb72b1cee452b1407288c398209ae8cc67
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-11-21 14:01:36 +0100

    add delete button

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ for (const button of buttons) {
     button.addEventListener("click", () => {
         let value = button.value;
         
-		if (value !== 'AC' && value !== '=') {
+		if (value !== 'AC' && value !== '=' && value !== "delete") {
 			display.textContent += value;
 		}
 		if (value === "AC") {
 			display.textContent = clear();
 		}
+        if (value === "delete") {
+			display.textContent = display.textContent.slice(0, -1);
+        }
 
 		if (value === '=') {
 			display.textContent = calculate(display.textContent);
